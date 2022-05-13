2333f8dd044f4db6d9018f2dd055d877dcf7fe86
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-05-13 18:19:25 +0200

    Update Program.cs

--- src/IeeeXploreScraper/IeeeXploreScraper/Program.cs
+++ src/IeeeXploreScraper/IeeeXploreScraper/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Threading;
 
@@ -12,11 +13,25 @@ namespace IeeeXploreScraper
         {
             SetupDriver();
 
-            GoToUrl("https://www.google.com/");
+            // Read links from file and modify them
+            // Go to each link in file
+            // Store value in file
+            // Go to next page
+
+            GoToUrl("https://ieeexplore.ieee.org/document/743356/");
+
+            int fullTextViews = GetElementValue();
+
+            Console.WriteLine(fullTextViews);
 
             CloseDriver();
 
             Console.ReadKey(true);
+        }
+
+        private static int GetElementValue()
+        {
+            return Convert.ToInt32(chromeDriver.FindElement(By.CssSelector("#LayoutWrapper > div > div > div > div.ng2-app > div > xpl-root > div > xpl-document-details > div > div.document-main.global-content-width-w-rr > section.document-main-header.row > div > xpl-document-header > section > div.document-header-inner-container.row > div > div > div.document-main-subheader > div.document-header-metrics-banner.row > div.document-banner.col.stats-document-banner > div.document-banner-metric-container.row > button:nth-child(2) > div.document-banner-metric-count")).Text);
         }
 
         private static void SetupDriver()
